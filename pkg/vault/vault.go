3e5b79c4e36b76e11cae57845d6852667c772adf
hossam <hossam@banquemisr>
2024-12-21 17:01:57 +0200

    using map

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -1,30 +1,17 @@
 package vault
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
 	"net/http"
-	"os"
+	Json "telnetapp/pkg/json"
 )
 
 func GetVaultToken(w http.ResponseWriter, r *http.Request) {
-	// open the file
-	file, err := os.Open("app/dummy.json")
-
-	if err != nil {
-		panic(err)
-	}
-
-	// read the file
-	data, err := io.ReadAll(file)
-	if err != nil {
-		panic(err)
-	}
 	// Cache the JSON content
-	cachedJSON := data
-	fmt.Println(r.URL.Path, r.Host)
+	cachedJSON := Json.VaultJson
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(cachedJSON)
+	json.NewEncoder(w).Encode(cachedJSON)
 
 }

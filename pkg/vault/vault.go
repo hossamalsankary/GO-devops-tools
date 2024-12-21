e8fdbf99aff879ffbc99a3dc47690953bea12296
hossam <hossam@banquemisr>
2024-12-21 17:39:56 +0200

    remote ip

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -3,16 +3,54 @@ package vault
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
+	"strings"
 	Json "telnetapp/pkg/json"
 )
 
 func GetVaultToken(w http.ResponseWriter, r *http.Request) {
+
+	// Restrictive access
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	host := strings.Split(r.Host, ":")[0]
+	fmt.Printf("Host: %v\n", host)
+	ip := os.Getenv("VAULT_IPS")
+	fmt.Printf("IP: %v\n", ip)
+	ipList := strings.Split(ip, ",")
+	remoteAddr := r.RemoteAddr
+	host, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		fmt.Printf(host, err)
+	}
+
+	// var ok bool
+
+	// for i := range ipList {
+	// 	if host == strings.TrimSpace(ipList[i]) {
+	// 		ok = true
+
+	// 	} else {
+
+	// 		ok = false
+	// 	}
+	// }
+	fmt.Printf("IP List: %v\n", ipList)
+	fmt.Println(host)
+
+	// if !ok {
+	// 	http.Error(w, "Forbidden", http.StatusForbidden)
+	// 	return
+	// }
 	// Cache the JSON content
 	cachedJSON := Json.VaultJson
 
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Printf(r.Host)
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(cachedJSON)
 
